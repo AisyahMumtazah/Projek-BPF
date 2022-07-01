60000ad90e6a45f15acac15a5b4940e9dc7cd2dc
Aisyah-Mumtazah <aisyah20ti@mahasiswa.pcr.ac.id>
2022-07-01 11:09:09 +0700

    header&footer + lainnya

---
+++ application/views/blank.php
@@ -0,0 +1,13 @@
+ <!-- Content -->
+
+ <h4 class="fw-bold p-4">Blank Page</h4>
+
+ <!-- / Content -->
+
+ <div class="buy-now">
+   <a
+     href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
+     target="_blank"
+     class="btn btn-danger btn-buy-now"
+     >Upgrade to Pro</a>
+ </div>
