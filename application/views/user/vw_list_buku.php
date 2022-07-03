4f35cbf3d15b3b78be57e1ee814cd4ed10eb177c
Aisyah-Mumtazah <aisyah20ti@mahasiswa.pcr.ac.id>
2022-07-03 19:23:40 +0700

    Update vw_list_buku.php

--- application/views/user/vw_list_buku.php
+++ application/views/user/vw_list_buku.php
@@ -1,6 +1,6 @@
 <div class="container">
     <br class="my-2">
-    <div class="row row-cols-1 row-cols-md-3 g-4 mb-5">
+    <div class="row row-cols-1 row-cols-md-5 g-4 mb-5">
         <div class="col">
             <div class="card h-100" role="card">
                 <img class="card-img-top" src="<?=base_url('assets/') ?>/assets/img/elements/2.jpg" alt="Card image cap">
