8c6867f890716a9ac0e24afd200fb7603575f11a
Aisyah-Mumtazah <aisyah20ti@mahasiswa.pcr.ac.id>
2022-08-04 01:19:33 +0700

    dashboard DLL

--- application/views/user/vw_list_buku.php
+++ application/views/user/vw_list_buku.php
@@ -1,11 +1,12 @@
 <div class="container">
     <br class="my-2">
+    <h1 class="card-header"><?php echo $judul; ?></h1>
     <div class="row row-cols-1 row-cols-md-5 g-4 mb-5">
         <?php $i=1;?>
         <?php foreach ($buku as $bk): ?>
         <div class="col">
             <div class="card h-100" role="card">
-                <img class="card-img-top" src="<?=base_url('assets/') ?>/assets/img/elements/2.jpg" alt="Card image cap">
+                <img class="card-img-top" src="<?= base_url('assets/img/buku/') . $bk['cover']; ?>" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title"><?=$bk['judul']?></h5>
                     <p class="card-text">
